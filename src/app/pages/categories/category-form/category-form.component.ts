6300df6d76261c878ad8d0c153eedaa56968395e
Tiago Yamamoto <japa@rede5.com.br>
2021-07-20 16:39:53 -0300

    completing the category module

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -132,7 +132,7 @@ export class CategoryFormComponent implements OnInit {
     if (error.status === 422)
       this.serverErrorMessages = JSON.parse(error._body).errors;
     else
-      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, teste mais tarde."]
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."]
   }
 
 }
