c724f95bd0f77a8f52de1f84083ac9177517fb61
Tiago Yamamoto <japa@rede5.com.br>
2022-02-08 00:38:25 -0300

    project restructuring

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,7 +1,5 @@
-
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 
@@ -14,9 +12,8 @@ import { IMaskModule } from "angular-imask";
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
   ],
